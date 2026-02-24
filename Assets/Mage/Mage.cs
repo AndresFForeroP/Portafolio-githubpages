fc53e81c5835fe60c891e9d4e773ca8e60db7f14
AndresFForeroP <andres2200500@correo.uis.edu.co>
2026-02-24 10:51:28 -0500

    feat: :sparkles: get basic assets

---
+++ Assets/Mage/Mage.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using DG.Tweening;
+public class Mage : MonoBehaviour
+{
+    public float bobAmount = 3f;      // píxeles (si usas PPU 100, esto sería 0.03f en mundo)
+    public float duration = 1.2f;
+
+    private Vector3 startPos;
+    private Vector3 startScale;
+
+    void Start()
+    {
+        startPos = transform.localPosition;
+        startScale = transform.localScale;
+
+        transform.DOLocalMoveY(startPos.y + 0.03f, duration)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo);
+
+        transform.DOScale(startScale * 1.01f, duration)
+            .SetEase(Ease.InOutSine)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+}
